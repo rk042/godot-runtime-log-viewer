e38645ba98c05ac2ac2819d7ebfb87b4606353bc
rk042 <rathodketan042@gmail.com>
2026-01-31 16:36:57 +0530

    added button click and logic to clean logs and close window

--- godot-rutime-log-viewer/scripts/log_viewer_canvas_scene.gd
+++ godot-rutime-log-viewer/scripts/log_viewer_canvas_scene.gd
@@ -2,14 +2,32 @@ extends Control
 
 @export var item:PackedScene
 @onready var v_box_container: VBoxContainer = $VBoxContainer/bottom_menu/ScrollContainer/VBoxContainer
+@onready var button_clean: Button = $VBoxContainer/top_menu/HBoxContainer/button_clean
+@onready var button_settings: Button = $VBoxContainer/top_menu/HBoxContainer/button_settings
+@onready var button_close: Button = $VBoxContainer/top_menu/HBoxContainer/button_close
 
 func _ready() -> void:
 	LogRouter.log_message.connect(_on_log_message)
 	LogRouter.log_error.connect(_on_log_error)
 	
+	button_clean.pressed.connect(_clean_button_pressed)
+	button_settings.pressed.connect(_settings_button_pressed)
+	button_close.pressed.connect(_close_button_pressed)
+	
 	for i in range(100):
 		print("testing message",i)
 
+func _close_button_pressed() -> void:
+	self.visible = false
+	pass
+func _clean_button_pressed() -> void:
+	for i in v_box_container.get_children():
+		i.queue_free()
+	pass
+func _settings_button_pressed() -> void:
+	print("coming soon!")
+	pass	
+	
 func _on_log_message(message: String, is_error: bool) -> void:
 	var _item:item_lable_button = item.instantiate()
 	v_box_container.add_child(_item)
